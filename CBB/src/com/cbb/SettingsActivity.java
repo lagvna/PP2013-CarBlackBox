4639d46673127b13e8e93846341404ac81402800
lagvna <lagvna@gmail.com>
2014-01-07 15:44:12 +0100

    v0.55

--- CBB/src/com/cbb/SettingsActivity.java
+++ CBB/src/com/cbb/SettingsActivity.java
@@ -2,21 +2,41 @@ package com.cbb;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
-public class SettingsActivity extends Activity {
-
+public class SettingsActivity extends Activity implements OnSeekBarChangeListener	{
+	private SeekBar bar;
+	private TextView textProgress;
+    
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_settings);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.settings, menu);
-		return true;
-	}
-
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);     
+        
+        bar = (SeekBar)findViewById(R.id.movieLengthBar);
+        bar.setOnSeekBarChangeListener(this);
+        textProgress = (TextView)findViewById(R.id.movieLengthText);
+        
+    }
+    
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+    		boolean fromUser) {
+    	textProgress.setText("Długość nagrania: " + (int)(progress + 10 )+ " sekund");
+    }
+    
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    	//textAction.setText("Ustawianie!");
+    	
+    }
+    
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    	seekBar.setSecondaryProgress(seekBar.getProgress());
+    	//textAction.setText("Gotowe");    	
+    }
 }
